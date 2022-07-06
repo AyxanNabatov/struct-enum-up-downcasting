da8691bb04cca2f730a19aa6db23df2bc84bd1f3
AyxanNabatov <107032159+AyxanNabatov@users.noreply.github.com>
2022-07-06 07:43:09 +0400

    Add files via upload

---
+++ stract enum downcastin upcasting/stract enum downcasting upcasting/stract enum downcasting upcasting/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace stract_enum_downcasting_upcasting
+{
+    class Program
+    {
+        private static int switch_on;
+
+        private static void Main(string[] args)
+        {
+            #region params
+
+            //    //int[] arr = { 2, 4, 5, 6, 7 };
+            //    GetElements(1, 2, 4, 6, 8, 9);
+
+            //    Sum();
+            //
+
+            //public static void GetElements(params int[] nums)
+            //{
+            //    foreach(var item in nums)
+            //    {
+            //        Console.WriteLine(item);
+            //    }
+            //}
+            //static void Sum(int[] arr)
+            //{
+
+            //}
+            #endregion params
+        }
+
+     static void CheckRole(int roleId)
+             
+        {
+        //    switch (roleId)
+        //    {
+        //        case (int)Week.SuperAdmin:
+        //            Console.WriteLine("Super admin");
+        //            break;
+        //        case (int)Week.Admin:
+        //            Console.WriteLine("Admin");
+        //            break;
+        //        case (int)Week.Member:
+        //            Console.WriteLine("Member");
+        //            break;
+        //        default:
+        //            Console.WriteLine("Not found role Id");
+        //            break;
+        //    }
+        }
+
+
+        
+
+
+             
+
+       
+    }
+    //public enum Week
+    //{
+    //    SuperAdmin,
+    //    Admin,
+    //    Member,
+    //}
+    
+
+    class Test
+    {
+        public int Number { get; set; }
+    }
+}
